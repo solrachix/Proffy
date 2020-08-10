9814853aefa31fb289d8fb9651a93dbac0b6684a
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2020-08-10 16:58:45 -0300

    supporting API versioning

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -7,7 +7,7 @@ const port = process.env.PORT_APP || 3333
 
 app.use(cors())
 app.use(express.json())
-app.use(routes)
+app.use('v1', routes)
 
 app.listen(port, () => {
   console.log(`Server running in port: ${port}`)
