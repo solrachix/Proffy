8fe45dd455328a57314221442e4d6aab6596301c
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2020-08-12 13:07:57 -0300

    creating function to return days of the week

---
+++ packages/web/src/utils/daysOfTheWeek.ts
@@ -0,0 +1,11 @@
+export function daysOfTheWeek () {
+  return [
+    { value: '0', label: 'Domingo' },
+    { value: '1', label: 'Segunda-feira' },
+    { value: '2', label: 'Terça-feira' },
+    { value: '3', label: 'Quarta-feira' },
+    { value: '4', label: 'Quinta-feira' },
+    { value: '5', label: 'Sexta-feira' },
+    { value: '6', label: 'Sábado' }
+  ]
+}
