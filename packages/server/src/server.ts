b94f0cc519ebce2fa7691084a63338650ca222e9
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2020-08-15 06:43:16 -0300

    create: static route to serve the media files

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -4,6 +4,7 @@ import express from 'express'
 import cors from 'cors'
 import routes from './routes'
 
+import path from 'path'
 require('dotenv').config()
 
 const app = express()
@@ -12,6 +13,8 @@ const port = process.env.PORT_APP || 3333
 app.use(cors())
 app.use(express.json())
 app.use('/v1', routes)
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(port, () => {
   console.log(`Server running in port: ${port}`)
